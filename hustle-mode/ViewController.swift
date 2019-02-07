988ea5409cb2576e2060bb10a9086693e9a5df7b
adiaz@mcclatchyservices.com <adiaz@mcclatchyservices.com>
2019-02-07 08:53:04 -0500

    Initial Commit

---
+++ hustle-mode/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  hustle-mode
+//
+//  Created by Andres Diaz on 2/7/19.
+//  Copyright © 2019 Andres Diaz. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
